8144e7ea142fba0c4a5eac482460630fc9b3ee45
AhmedHamdyYassin12 <ahmedhamdyyassin12@gmail.com>
2022-10-13 01:35:35 +0200

    info about process.exit()

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     console.log(req);
+    /**
+     * process exit is a sign to the listener to stop listing to requests
+     * because you are telling the server i don't need the event loop any more
+     * yes btw the event loop is kind of multi threading somehow to keep always the server
+     * in a steady state to listen to new request and by using the process.exit() to tells i am not
+     * interested any more !
+     */
+    // process.exit();
 });
 
 server.listen(3000);
