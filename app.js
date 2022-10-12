df5dbb0126fcc3680a5b224991b6ccb6118624c5
AhmedHamdyYassin12 <ahmedhamdyyassin12@gmail.com>
2022-10-13 01:22:20 +0200

    create a server in a simple steps

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/**
+ * here is how to create the server in a simple steps
+ * we needed to use the http module and from it we used
+ * the create server method that needs a function with req and res 
+ * finally we needed to listen to a port that is why we used the listen 
+ */
 const http = require("http");
 
 const server = http.createServer((req, res) => {
